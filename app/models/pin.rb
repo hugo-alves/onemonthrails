4cfdc7705c454beb0611e74db593bbdfbbb060d4
hugo.alves <goninja@gmail.com>
2014-11-10 12:17:29 +0000

    desliguei os timestamps do paperclip e ja mostra as imagens

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :use_timestamp => false, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage/
 
 end
