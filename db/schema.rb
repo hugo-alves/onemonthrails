bd83bf4618632880ae1ce00bca921b86de375e4b
hugo.alves <goninja@gmail.com>
2014-11-20 09:45:54 +0000

    fiz db:migrate depois de update do brew

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,6 @@ ActiveRecord::Schema.define(version: 20141109184114) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "provider"
-    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
